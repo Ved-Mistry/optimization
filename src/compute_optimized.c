041d1dc9a5013cd9b6be7b8825db0567993841ad
Class Account <cs61c-azx@imail.EECS.Berkeley.EDU>
2023-04-27 21:33:07 -0700

    finished

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -6,8 +6,17 @@
 // Computes the dot product of vec1 and vec2, both of size n
 int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   // TODO: implement dot product of vec1 and vec2, both of size n
-
-  return -1;
+  int result[8];
+  __m256i prod_v = __mm_set1_epi32(0);
+  for (int i = 0; i < n/8 * 8; i += 8) { // Vectorized loop
+        __m256i temp = __mm_mullo_epi32(__mm_loadu_si256((__m256i *) (vec1 + i)), __mm_loadu_si256((__m256i *) (vec2 + i)));
+        prod_v = _mm_add_epi32(prod_v, temp); 
+  }
+  mm_storeu_si128((__m256i *) result, prod_v);
+  for (int i = n/4 * 4; i < n; i++) { // Handle tail case
+  result[0] *= vec1[i]*vec2[i];
+  }
+  return result[0] + result[1] + result[2] + result[3] + result[4] + result[5] + result[6] + result[7];
 }
 
 // Computes the convolution of two matrices
